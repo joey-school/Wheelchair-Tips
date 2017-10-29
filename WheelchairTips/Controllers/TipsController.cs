42c059f6a79b7d471c97efdb8028043de10b5838
Joey <joeyhoogerwerf@gmail.com>
2017-10-29 18:06:27 +0000

    - added tip details page

--- WheelchairTips/Controllers/TipsController.cs
+++ WheelchairTips/Controllers/TipsController.cs
@@ -69,6 +69,7 @@ namespace WheelchairTips.Controllers
 
             var tip = await _context.Tip
                 .Include(t => t.Category)
+                .Include(t => t.ApplicationUser)
                 .SingleOrDefaultAsync(m => m.Id == id);
             if (tip == null)
             {
@@ -76,123 +77,6 @@ namespace WheelchairTips.Controllers
             }
 
             return View(tip);
-        }
-
-        // GET: Tips/Create
-        [Authorize]
-        public IActionResult Create()
-        {
-
-
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name");
-            return View();
-        }
-
-        // POST: Tips/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Content,CategoryId")] Tip tip)
-        {
-            if (ModelState.IsValid)
-            {
-                tip.ApplicationUserId = _userManager.GetUserId(HttpContext.User);
-                _context.Add(tip);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id", tip.CategoryId);
-            return View(tip);
-        }
-
-        // GET: Tips/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tip = await _context.Tip.SingleOrDefaultAsync(m => m.Id == id);
-            if (tip == null)
-            {
-                return NotFound();
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id", tip.CategoryId);
-            return View(tip);
-        }
-
-        // POST: Tips/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,Author,CategoryId")] Tip tip)
-        {
-            if (id != tip.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(tip);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TipExists(tip.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Index");
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id", tip.CategoryId);
-            return View(tip);
-        }
-
-        // GET: Tips/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tip = await _context.Tip
-                .Include(t => t.Category)
-                .SingleOrDefaultAsync(m => m.Id == id);
-            if (tip == null)
-            {
-                return NotFound();
-            }
-
-            return View(tip);
-        }
-
-        // POST: Tips/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var tip = await _context.Tip.SingleOrDefaultAsync(m => m.Id == id);
-            _context.Tip.Remove(tip);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-
-        private bool TipExists(int id)
-        {
-            return _context.Tip.Any(e => e.Id == id);
         }
     }
 }
